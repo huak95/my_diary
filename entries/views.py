afd9d50c850e61c746dd15fec2ed86262a138eb3
huak95 <huak95@gmail.com>
2023-06-01 15:46:51 +0700

    update entry views

--- entries/views.py
+++ entries/views.py
@@ -1,3 +1,5 @@
+from typing import Any
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import (
@@ -8,6 +10,9 @@ from django.views.generic import (
     DeleteView,
 )
 
+from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
+
 from .models import Entry
 
 class EntryListView(ListView):
@@ -17,21 +22,28 @@ class EntryListView(ListView):
 class EntryDetailView(DetailView):
     model = Entry
 
-class EntryCreateView(CreateView):
+class EntryCreateView(SuccessMessageMixin, CreateView):
     model = Entry
     fields = ['title', 'content']
     success_url = reverse_lazy('entry-list')
+    success_message = "Your new entry was created!"
 
-class EntryUpdateView(UpdateView):
+class EntryUpdateView(SuccessMessageMixin, UpdateView):
     model = Entry
     fields = ['title', 'content']
+    success_message = "Your entry was updated!"
 
     def get_success_url(self):
         return reverse_lazy(
             'entry-detail',
-            kwargs={'pk': self.entry.id}
+            kwargs={'pk': self.object.pk}
         )
     
 class EntryDeleteView(DeleteView):
     model = Entry
     success_url = reverse_lazy('entry-list')
+    success_message = "Your entry was deleted!"
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, self.success_message)
+        return super().delete(request, *args, **kwargs)
